c4973d72cfa0b5ca0bf3417301f4a7146e18dc34
Timothy Chi <microchi@gmail.com>
2021-06-05 11:59:33 +0800

    dev: Add a Div Element in Logic Button

--- make.sh
+++ make.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-DT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
-if [ "$1" = "debug" ]; then
-    DEBUG="debug"
-else
+# DT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
+# if [ "$1" = "debug" ]; then
+#     DEBUG="debug"
+# else
     OUT_DIR=$1
-    DEBUG=$2
-fi
+#     DEBUG=$2
+# fi
 
 # If not run from DataTables build script, redirect to there
-if [ -z "$DT_BUILD" ]; then
-    cd $DT_DIR/build
-    ./make.sh extension SearchBuilder $DEBUG
-    cd -
-    exit
-fi
+# if [ -z "$DT_BUILD" ]; then
+#     cd $DT_DIR/build
+#     ./make.sh extension SearchBuilder $DEBUG
+#     cd -
+#     exit
+# fi
 
 # Change into script's own dir
-cd $(dirname $0)
+# cd $(dirname $0)
 
-DT_SRC=$(dirname $(dirname $(pwd)))
-DT_BUILT="${DT_SRC}/built/DataTables"
-. $DT_SRC/build/include.sh
+# DT_SRC=$(dirname $(dirname $(pwd)))
+# DT_BUILT="${DT_SRC}/built/DataTables"
+# . $DT_SRC/build/include.sh
 
 npm install
 
